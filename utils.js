80e61ea7ece52d8c4c1d9043cc3298d664090d3e
javiscripto <javier.mecker94@gmail.com>
2023-11-30 16:30:10 -0300

    implementacion de middlewares validacion de roles para acceso a rutas y operaciones

--- utils.js
+++ utils.js
@@ -15,7 +15,7 @@ export const activeSession=(req, res, next)=>{
       return  res.redirect("/api/sessions/login")
     }
 }
-//middleware que valida el rol 
+//middleware que valida el rol para crear nuevos productos 
 export const isAdmin=(req, res, next)=>{
     console.log(req.session)
    if(req.session.user.role!=="admin"){
@@ -25,6 +25,7 @@ export const isAdmin=(req, res, next)=>{
    }
    
 }
+//middleware valida rol para agregar productos al carrito
 export const isUser=(req, res, next)=>{
     if(req.session.user.role!=="user"){
         res.send("solo los usuarios pueden realizar esta operacion")
